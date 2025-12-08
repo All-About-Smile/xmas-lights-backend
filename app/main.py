3cd48b0effa38d9aedae1928aa7f33f2140a9baa
박종윤 <65222636+La-Coruna@users.noreply.github.com>
2025-12-08 21:36:56 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.auth import router as auth_router
 from app.api.capsule import router as capsule_router
 from app.api.letters import router as letters_router
@@ -7,7 +9,20 @@ from app.core.config import settings
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(public_router)
 app.include_router(auth_router)
 app.include_router(capsule_router)
 app.include_router(letters_router)
