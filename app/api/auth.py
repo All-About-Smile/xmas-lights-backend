3587207c653a0e49bdc8d4ba9a4e7a7de4430fac
박종윤 <65222636+La-Coruna@users.noreply.github.com>
2025-12-10 10:28:16 +0900

    로그인 API 만들기

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from app.db.session import get_db
+
+from app.core.security import (create_access_token, hash_password,
+                               verify_password)
 from app.db.models.user import User
+from app.db.session import get_db
+from app.schemas.auth_schema import LoginRequest, TokenResponse
 from app.schemas.user_schema import UserCreate, UserRead
-from app.core.security import hash_password
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -25,3 +28,26 @@ def signup(payload: UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(user)
     return user
+
+@router.post("/login", response_model=TokenResponse)
+def login(payload: LoginRequest, db: Session = Depends(get_db)):
+    # 존재하는 이메일인가?
+    user = db.query(User).filter(User.email == payload.email).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid email or password",
+        )
+
+    # 비밀번호 검증
+    if not verify_password(payload.password, user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid email or password",
+        )
+
+    # JWT 생성 → sub에 user.id 넣기
+    token_data = {"sub": str(user.id)}
+    access_token = create_access_token(token_data)
+
+    return TokenResponse(access_token=access_token)
